e9fffc94e42b01470049f4ade6f8729d5eae2b7a
geterns <geterns@163.com>
2013-12-05 05:55:50 -0800

    update sample code

--- C++/PluginTest/PluginTest.cpp
+++ C++/PluginTest/PluginTest.cpp
@@ -9,41 +9,55 @@
 // from the SDK docs, uncomment the function(s). Otherwise leave them
 // commented out (or get rid of them)!
 
-struct Measure
-{
-	Measure() {}
+class measure_data{
+
+private:
+
+
+public:
+
+
 };
 
-PLUGIN_EXPORT void Initialize(void** data, void* rm)
-{
-	Measure* measure = new Measure;
-	*data = measure;
+// called when skin is launched/refreshed
+// read local skin file to create Rainmeter runtime measure object, which holds
+// data of the measure section, including its name, all the "option=value"
+// pairs specified in the local skin file, and so on.
+// use the RmReadString/RmReadInt-like functions to read value of a option from
+// this object.
+PLUGIN_EXPORT void Initialize(void **data, void *rm){
+	// you can allocate a data block to store any data of the measure(plugin)
+	// like this, and the data pointer is kept by Rainmeter.
+	*data = reinterpret_cast<void *>(new measure_data);
 }
 
-PLUGIN_EXPORT void Reload(void* data, void* rm, double* maxValue)
-{
-	Measure* measure = (Measure*)data;
+// called after Initialize.
+// while dynamic variables is enabled, the Rainmeter runtime measure object can
+// be changed in runtime, and this function will be called on every update cycle
+// before Reload to synchronize some data.
+PLUGIN_EXPORT void Reload(void* data, void* rm, double* maxValue){
+	
 }
 
-PLUGIN_EXPORT double Update(void* data)
-{
-	Measure* measure = (Measure*)data;
+// call on every update cycle
+PLUGIN_EXPORT double Update(void* data){
+	
 	return 0.0;
 }
 
-PLUGIN_EXPORT LPCWSTR GetString(void* data)
-{
-	Measure* measure = (Measure*)data;
+// return a string representation of the measure
+PLUGIN_EXPORT LPCWSTR GetString(void* data){
+
 	return L"";
 }
 
-PLUGIN_EXPORT void ExecuteBang(void* data, LPCWSTR args)
-{
-	Measure* measure = (Measure*)data;
+// 
+PLUGIN_EXPORT void ExecuteBang(void* data, LPCWSTR args){
+	
 }
 
-PLUGIN_EXPORT void Finalize(void* data)
-{
-	Measure* measure = (Measure*)data;
-	delete measure;
+// 
+PLUGIN_EXPORT void Finalize(void* data){
+	// release data block
+	delete reinterpret_cast<measure_data *>(data);
 }
